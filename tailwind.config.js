c5a65373595880e905801bd7dab7e4e629bbd902
Nolb226 <hn26677@gmail.com>
2024-05-16 22:11:24 +0700

    chore: adding style and delete App.test

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{js,jsx,ts,tsx}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-};
+    content: ['./src/**/*.{js,jsx,ts,tsx}'],
+    theme: {
+        extend: {
+            colors: {
+                bgPrimary: '#001232', // background of container
+                bgPrimaryLayer: 'rgba(0, 18, 50, 0.8)', //#0A1E5ECC layer of image background
+                bgPrimaryBar: 'rgba(5, 17, 63, 1)', // header, navbar, footer, sidebar
+                lightPrimary: '#31AFD7', // toggle button, more icon
+                borderColor: '#314C81', // button background, border
+                gradientStart: 'rgba(255, 67, 67, 0.85)',
+                gradientMid: ' #AA52A1',
+                gradientStop: '#002DBB',
+                disabled: '#AA52A1',
+                highlight: '#31D7A9',
+            },
+            screens: {
+                sm: '680px',
+                md: '900px',
+                lg: '1024px',
+                xl: '1366px',
+            },
+        },
+    },
+    plugins: [],
+}
